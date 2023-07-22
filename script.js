5de774eb2e49d251707de71d2af42e3854a964e2
Ainme <a.ndayishim@alustudent.com>
2023-07-22 08:00:17 +0200

    done

---
+++ script.js
@@ -0,0 +1,28 @@
+currentValue = ''
+currentOperator = ''
+
+function updateDisplay(clickedButton) {
+    let operationDisplay = document.querySelector(".operation-display");
+    currentValue += clickedButton.textContent
+    operationDisplay.textContent = currentValue
+}
+
+function setOperator(clickedButton) {
+    let operationDisplay = document.querySelector(".operation-display");
+    currentOperator = clickedButton.textContent
+    currentValue += currentOperator
+    operationDisplay.textContent = currentValue
+}
+
+function calculate() {
+    let resultDisplay = document.querySelector(".result-display");
+    results = eval(currentValue)
+    resultDisplay.textContent = results
+}
+
+function clearDisplay() {
+    let resultDisplay = document.querySelector(".result-display");
+    let operationDisplay = document.querySelector(".operation-display");
+    resultDisplay.textContent = ''
+    operationDisplay.textContent = ''
+}
